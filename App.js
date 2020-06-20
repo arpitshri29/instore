13111d3d47522336d6487b0870be2b529b4083f7
Arpit <arpit@interfacem.com>
2020-06-20 13:09:32 +0530

    Mobile app Instore

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Alert, Button, TouchableWithoutFeedback, TouchableHighlight, TouchableOpacity, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Arpit!</Text>
+      <Button title="click me!"
+              onPress={() => Alert.alert("My title", "My message", [
+                {text: "Yes", onPress: () => console.log("Pressed Yes")},
+                {text: "No", onPress: () => console.log("Pressed No")},
+              ])}/>
+      <TouchableHighlight onPress={() => console.log("Image tapped!")}>
+        <Image
+            source={{
+              width: 200,
+              height: 300,
+              uri: "https://picsum.photos/200/300",
+            }} />
+      </TouchableHighlight>
+    </SafeAreaView>
   );
 }
 
